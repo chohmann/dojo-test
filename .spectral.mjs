83e179446666acf8d0a4c0e48aa9a280392389fd
Chelsea Hohmann <chelsea@stoplight.io>
2023-06-20 13:50:02 -0500

    Modified 7 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwNjg0Mw/spectral.js?branch_slug=main&token=b865b956-d8e7-4054-9b98-560879e183f1";
+export default { extends: ruleset };
